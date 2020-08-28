2f1f9543f91fc64aedab3d458e0125f12a799943
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-08-29 00:05:49 +0700

    components part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, FlatList } from "react-native";
+import { ListItem } from "react-native-elements";
+
+function Menu(props) {
+  const renderMenuItem = ({ item, index }) => {
+    return (
+      <ListItem
+        key={index}
+        title={item.name}
+        subtitle={item.description}
+        hideChevron={true} //hide chevron on ios
+        leftAvatar={{ source: require("./images/uthappizza.png") }} //render image
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={props.dishes}
+      renderItem={renderMenuItem} //specified how to render each item in the list
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+}
+
+export default Menu;
