5e16cbeda8e296d3aae539fc4b5a82b8ae3467fb
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-08-31 23:45:32 +0700

    buttons and icons

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Main from "./components/MainComponent";
+import { StatusBar } from "react-native";
 
 export default class App extends React.Component {
   render() {
+    StatusBar.setBarStyle("light-content", true);
     return <Main />;
   }
 }
