735559f109391fb45dbc7db46a27fb0d72359c19
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-09-05 00:25:39 +0700

    persist redux store

--- App.js
+++ App.js
@@ -3,15 +3,19 @@ import Main from "./components/MainComponent";
 import { StatusBar } from "react-native";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { Loading } from "./components/LoadingComponent";
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default class App extends React.Component {
   render() {
     StatusBar.setBarStyle("light-content", true);
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
